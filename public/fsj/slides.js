8e7ff0e5ed4e5db03bfc62465fc87a7b9cd8e122
LarsM <lam@cphbusiness.dk>
2017-11-06 18:07:59 +0100

    Elective Presentations

---
+++ public/fsj/slides.js
@@ -0,0 +1,6 @@
+//require(['order!../../public/node-intro/slide_config', 'order!modernizr.custom.45394',
+  require(['order!../../fsj/slide_config', 'order!modernizr.custom.45394',
+         'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
+         'order!slide-deck'], function(someModule) {
+
+});
---
+++ public/sec/slide_config.js
@@ -0,0 +1,31 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'WEB Security',
+    subtitle: '',
+    date: 'Spring - 2018',
+     useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: '../images/cphBusinessFavIcon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+    ,
+    theme: ['../../../theme/css/myTemplate'] // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Kasper & Lars',
+    company: '',
+    gplus: '',
+    twitter: '',
+    www: '',
+    github: ''
+  }
+  ]
+};
+
