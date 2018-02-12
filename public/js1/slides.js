51f93f745e4ba7ef2ae242766aaece52cfb58add
lars-m <lam@cphbusiness.dk>
2018-02-12 10:32:33 +0100

    JS lesson 1;

--- public/js1/slides.js
+++ public/js1/slides.js
@@ -1,6 +1,7 @@
 //require(['order!../../public/node-intro/slide_config', 'order!modernizr.custom.45394',
-  require(['order!../../javascript1/slide_config', 'order!modernizr.custom.45394',
+  require(['order!../../js1/slide_config', 'order!modernizr.custom.45394',
          'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
          'order!slide-deck'], function(someModule) {
 
 });
+
