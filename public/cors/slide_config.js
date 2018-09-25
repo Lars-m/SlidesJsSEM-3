0ff781dd4240a879cac90990d378a322dbff3cce
lars-m <lam@cphbusiness.dk>
2018-09-25 16:20:20 +0200

    cors updates

--- public/cors/slide_config.js
+++ public/cors/slide_config.js
@@ -3,7 +3,7 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'SOP and CORS',
     subtitle: 'Single Origin Policy and CORS',
-    date: 'Spring - 2017',
+    date: 'Fall - 2018',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
